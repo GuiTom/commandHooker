5eda05d1fd84cc9c41f4357c97fd385a4ecd8423
chen.chao2 <chen.chao2@olaola.chat>
2022-05-11 13:11:56 +0800

    Initial Commit

---
+++ commandHooker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  commandHooker
+//
+//  Created by chenchao on 2022/5/11.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
